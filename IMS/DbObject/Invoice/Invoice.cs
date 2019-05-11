6f91a026e8e2460255d53988c7eb47fd26d02c59
mr360 <qd0097@gmail.com>
2019-05-11 21:38:40 +1000

    Report and Invoice shell created

---
+++ IMS/DbObject/Invoice/Invoice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.DbObject.Invoice
+{
+    public class Invoice : DbObject
+    {
+        public Invoice(string id) : base(id)
+        {
+        }
+
+        public override string View
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ IMS/DbObject/Report/Report.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.DbObject.Report
+{
+    public class Report : DbObject
+    {
+        public Report(string id) : base(id)
+        {
+        }
+
+        public override string View
+        {
+            get
+            {
+                return "The report contains the following information:";
+            }
+        }
+    }
+}
