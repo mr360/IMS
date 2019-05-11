231827e2d04816839d71caaca7ba13bb3712d35e
mr360 <qd0097@gmail.com>
2019-05-11 19:05:54 +1000

    DbObject added

---
+++ IMS/DbObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS
+{
+    public abstract class DbObject
+    {
+        private string _id;
+
+        public DbObject(string id)
+        {
+            _id = id;
+        }
+
+        public string Id
+        {
+            get 
+            {
+                return _id;
+            }
+        }
+
+        public abstract string View { get; }
+         
+    }
+}
