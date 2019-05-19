5f9de5c0025ee4b14215d7a74d180b671b65e6dd
mr360 <qd0097@gmail.com>
2019-05-20 03:42:18 +1000

    Added Idb implementation

--- IMS/Database/DbTable.cs
+++ IMS/Database/DbTable.cs
@@ -17,5 +17,72 @@ namespace IMS.Database
             _name = name;
         }
 
+        public List<string> View
+        {
+            get
+            {
+                List<string> temp = new List<string>();
+                foreach(DbObject.DbObject itm in _table)
+                {
+                    temp.Add(itm.Id);
+                }
+                return temp;
+            }
+        }
+
+        public string Create(DbObject.DbObject item)
+        {
+            if (item == null)
+            {
+                return "Not valid item.";
+            }
+            if(_table.Contains(item))
+            {
+                return "Duplication! Item already exists.";
+            }
+
+            _table.Add(item);
+            return "Successfully added.";
+        }
+
+        public string Delete(string id)
+        {
+            foreach (DbObject.DbObject itm in _table)
+            {
+                if (itm.Id == id)
+                {
+                    _table.Remove(itm);
+                    return "Successfully deleted.";
+                }
+            }
+
+            return "No such item found.";
+        }
+
+        public DbObject.DbObject Read(string id)
+        {
+            foreach (DbObject.DbObject item in _table)
+            {
+                if (item.Id == id)
+                {
+                    return item;
+                }
+            }
+
+            return null;
+        }
+
+        public string Update(DbObject.DbObject item)
+        {
+            int index = _table.IndexOf(Read(item.Id));
+
+            if (!(index == -1))
+            {
+                _table[index] = item;
+                return "Successfully updated.";
+            }
+
+            return "No such item found";
+        }
     }
 }
