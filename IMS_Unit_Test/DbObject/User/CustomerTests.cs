c394e093e4bcdde7177451e31791f8595836fe96
mr360 <qd0097@gmail.com>
2019-05-20 08:27:16 +1000

    Implemented tests, added tests

--- IMS_Unit_Test/DbObject/User/CustomerTests.cs
+++ IMS_Unit_Test/DbObject/User/CustomerTests.cs
@@ -11,10 +11,36 @@ namespace IMS.User.Tests
     [TestClass()]
     public class CustomerTests
     {
+        Customer c1 = new Customer("C0001", "Jackson", "25 Milwalki Street, CA, 80010");
+
         [TestMethod()]
-        public void CustomerTest()
+        public void CustomerAccountTypeTest()
         {
-            Assert.Fail();
+            Assert.AreEqual("IMS.User.Customer", c1.AccountType.ToString());
+        }
+
+        [TestMethod()]
+        public void CustomerIDTest()
+        {
+            Assert.AreEqual("C0001", c1.Id);
+        }
+
+        [TestMethod()]
+        public void CustomerNameTest()
+        {
+            Assert.AreEqual("Jackson", c1.Name);
+        }
+
+        [TestMethod()]
+        public void CustomerAddressTest()
+        {
+            Assert.AreEqual("25 Milwalki Street, CA, 80010", c1.Address);
+        }
+
+        [TestMethod()]
+        public void CustomerViewTest()
+        {
+            Assert.AreEqual("Customer :Jackson | Address: 25 Milwalki Street, CA, 80010\n", c1.View);
         }
     }
 }
