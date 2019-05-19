0ae953fbdc1b6cfb8f6e20330408f2358e9069f9
mr360 <qd0097@gmail.com>
2019-05-20 07:40:15 +1000

    Added extra test func

--- IMS_Unit_Test/DbObject/AddonTests.cs
+++ IMS_Unit_Test/DbObject/AddonTests.cs
@@ -12,12 +12,43 @@ namespace IMS.Tests
     public class AddonTests
     {
         [TestMethod()]
-        public void AddonTest()
+        public void AddonAddCompatibleTest()
         {
             Addon a1 = new Addon("A0001", "BodyKit3", "The latest bodykit with seven carbon atoms", 155.0);
             //Add compatible vehicles 
-
-            
+            a1.AddCompatible("V0001");
+            a1.AddCompatible("V0002"); 
         }
+
+        [TestMethod()]
+        public void AddonDeleteCompatibleTest()
+        {
+        }
+
+        [TestMethod()]
+        public void AddonCheckCompatibleTest()
+        {
+        }
+
+        [TestMethod()]
+        public void AddonPriceTest()
+        {
+        }
+
+        [TestMethod()]
+        public void AddonDescTest()
+        {
+        }
+
+        [TestMethod()]
+        public void AddonNameTest()
+        {
+        }
+
+        [TestMethod()]
+        public void AddonViewTest()
+        {
+        }
+
     }
 }
