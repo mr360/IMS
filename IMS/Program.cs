6425b487a46bb2d7ddf1d277192903f38d937674
mr360 <qd0097@gmail.com>
2019-05-20 10:51:19 +1000

    Added sale code

--- IMS/Program.cs
+++ IMS/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using IMS.Invoice;
+using IMS.User;
+
 namespace IMS
 {
     class Program
@@ -12,6 +15,9 @@ namespace IMS
         {
             Bay x = new Bay("ddd");
             User.Staff s1 = new User.Staff("S000001", "Jack Jones",JobRole.Sale);
+             Vehicle v01 = new Vehicle("V00001", Brand.Audi, "MX-60", new DateTime(2008, 01, 01), 125000.00);
+             Staff s01 = new Staff("S0001", "Steve", JobRole.Sale);
+            Sale si01 = new Sale("SI0001", s01, v01);
             Console.WriteLine(s1.AccountType);
             Console.ReadLine();
         }
