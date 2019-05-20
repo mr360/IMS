ca9ed5e7c0a1d568e499d28614f556414bdebc72
mr360 <qd0097@gmail.com>
2019-05-21 09:46:22 +1000

    Implemented createsale method features

--- IMS/Instance/SaleInstance.cs
+++ IMS/Instance/SaleInstance.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using IMS.User;
+using IMS.Builder;
+using IMS.Invoice;
+using IMS.Manager;
 
 namespace IMS
 {
@@ -14,24 +17,52 @@ namespace IMS.Instance
 {
     public class SaleInstance
     {
-        public SaleInstance(Staff s)
+        private string _vehicleId;
+        private List<string> _addonIds;
+        private Vehicle _tradeIn;
+        Staff _saleRep;
+        Dictionary<string, IManager> _manager;
+
+        public SaleInstance(Staff s, VehicleManager vm, AddonManager am, InvoiceManager im)
         {
             if (s.Role != JobRole.Sale)
             {
                 throw new System.ArgumentException("Invalid User! Cannot create instance!");
             }
+
+            _saleRep = s;
+
+            _manager = new Dictionary<string, IManager>();
+            _manager.Add("Vehicle", vm);
+            _manager.Add("Addon", am);
+            _manager.Add("Invoice", im);
         }
-        public bool Add(IdType id, string vehicleId)
+        public bool Add(IdType type, string id)
         {
             //add vehicle id ; add addon id
-            //check if duplication is there
+            //check if duplication is there            
+            if (type == IdType.Addon)
+            {
+                if (_addonIds.Contains(id))
+                {
+                    return false;
+                }
+                _addonIds.Add(id);
+            }
+            else
+            {
+                _vehicleId = id;
+                return true;
+            }
+            
             return false;
         }
 
         public string Add(Vehicle tradeIn)
         {
-            // TODO make vehicle manager check validatity of vehicle
-            return "";
+            // optional validate vehicle here
+            _tradeIn = tradeIn;
+            return "Successfully added.";
         }
 
         public string ViewSelection
@@ -48,7 +79,31 @@ namespace IMS.Instance
             // Validate everything / run invoicebuilder , if error return err result
             // Add sale invoice to invoice db
             // Output the sale invoice id
-            return "";
+
+            VehicleBuilder vBuild = new VehicleBuilder(_manager["Vehicle"], _manager["Addon"]);
+            string lMsg = "";
+
+            lMsg = vBuild.Add(_vehicleId);
+            if (lMsg != "Success.")
+            {
+                return lMsg;
+            }
+
+            vBuild.Add(_addonIds);
+            if (lMsg != "Success.")
+            {
+                return lMsg;
+            }
+
+            Order orders = vBuild.Prepare();
+
+            InvoiceBuilder iBuild = new InvoiceBuilder();
+            iBuild.Staff = _saleRep;
+            iBuild.Order = orders;
+            iBuild.TradeVehicle = _tradeIn;
+            Sale sInvoice = iBuild.Prepare() as Sale;
+
+            return _manager["Invoice"].Add(sInvoice);
         }
 
     }
