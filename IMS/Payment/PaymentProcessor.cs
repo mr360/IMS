0db852fac8c31508907d0ce8ffc84391e880f3f1
mr360 <qd0097@gmail.com>
2019-05-21 03:06:06 +1000

    Created empty Payment processor

---
+++ IMS/Payment/PaymentProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS
+{
+    struct Card
+    {
+        string _name;
+        int _number;
+        int _securitycode;
+    }
+}
+
+namespace IMS.Payment
+{
+    public class PaymentProcessor
+    {
+    }
+}
