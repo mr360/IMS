39beefe8d58dcd4c782cb6ff13b34962e5be0252
mr360 <qd0097@gmail.com>
2019-05-22 04:18:05 +1000

    Added comment plan, and changed some methods defin

--- IMS/Instance/GarageInstance.cs
+++ IMS/Instance/GarageInstance.cs
@@ -10,7 +10,7 @@ namespace IMS.Instance
 {
     public class GarageInstance : Instance
     {
-        public GarageInstance(Staff s, VehicleManager vm, AddonManager am, BayManager bm) : base(vm,am,bm)
+        public GarageInstance(Staff s, VehicleManager vm, AddonManager am, BayManager bm) : base(vm, am, bm)
         {
             if (s.Role != JobRole.Garage)
             {
@@ -39,34 +39,78 @@ namespace IMS.Instance
             return _manager["Addon"].Add(a);
         }
 
-        string ViewTasks
+        public string ViewTasks
+        {
+            get
+            {
+                string ss = "";
+                ss += "SOLD VEHICLES THAT NEED TO BE REMOVED: \n";
+                // foreach vehicle in Vehicle Manager and tax-invoice vehicles, show as sold and need to remove from bay
+                ss += "UNALLOCATED VEHICLES THAT NEED TO BE ADDED TO BAYS: \n";
+                // foreach vehicle in Vehicle Manager and Bay Manager that does not have allocation, show and need to add to bay
+                ss += "TRADE-IN VEHICLES THAT NEED TO BE ADDED TO INVENTORY: \n";
+                // foreach each trade in vehicle in tax-invoices not in vehicle manager, show and add to system
+
+                return ss;
+            }
+        }
+
+
+        List<string> SoldVehicle
         {
             get
             {
                 // foreach vehicle in Vehicle Manager and tax-invoice vehicles, show as sold and need to remove from bay
-                // foreach vehicle in Vehicle Manager and Bay Manager that does not have allocation, show and need to add to bay
-                // foreach each trade in vehicle in tax-invoices not in vehicle manager, show and add to system
-                
+                throw new NotImplementedException();
             }
         }
 
-        string ViewOpenBays
+        List<string> UnallocatedVehicle
+        {
+            get
+            {
+                // foreach vehicle in Vehicle Manager and Bay Manager that does not have allocation, show and need to add to bay
+                throw new NotImplementedException();
+            }
+        }
+
+        List<string> NonProcessedTradeInVehicle
+        {
+            get
+            {
+                // foreach each trade in vehicle in tax-invoices not in vehicle manager, show and add to system
+                throw new NotImplementedException();
+            }
+        }
+
+        public string ViewOpenBays
         {
             get
             {
                 // show bays that are open
+                return "";
             }
         }
 
         public string AssignVehicleToBay(string bayId, string vehicleId)
         {
+            // Check if vehicleid of vehicle exists (vehicle manager)
+            // Check if bayid exists in Bay Manager (bay manager)
             // Get vehicle id and bayId
+            // check if bay has the vehicle and check if vehicle isn't in multiple bays (probs do this in BayManager)
         }
 
         public bool RemoveVehicleFromBay(string bayId)
         {
             // remove vehicle id from bay
+
         }
 
+        public bool RemoveSoldVehicle(string bayId)
+        {
+            //remove vehicle from inventory
+            // remove vehicle from bay
+            RemoveVehicleFromBay(bayId);
+        }
     }
 }
