54f86a4237746d202f1d4f65c577df19140a2610
mr360 <qd0097@gmail.com>
2019-05-21 13:04:49 +1000

    Created  base instance that deals with managers

---
+++ IMS/Instance/Instance.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IMS.Manager;
+
+namespace IMS.Instance
+{
+    public class Instance
+    {
+        protected Dictionary<string, IManager> _manager = new Dictionary<string, IManager>();
+        public Instance(VehicleManager vm, AddonManager am, InvoiceManager im)
+        {
+            _manager.Add("Vehicle", vm);
+            _manager.Add("Addon", am);
+            _manager.Add("Invoice", im);
+        }
+
+        public Instance(InvoiceManager im, UserManager um)
+        {
+            _manager.Add("Invoice", im);
+            _manager.Add("User", um);
+        }
+
+        public Instance(ReportManager rm)
+        {
+            _manager.Add("Report", rm);
+        }
+    }
+}
