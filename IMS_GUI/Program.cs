a46382c5ad8aa5633f9595b136da397fea6f35a7
mr360 <qd0097@gmail.com>
2019-05-22 23:33:07 +1000

    Added AddAddon and AddVehicle forms

--- IMS_GUI/Program.cs
+++ IMS_GUI/Program.cs
@@ -16,7 +16,7 @@ namespace IMS_GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new SaleInstance());
         }
     }
 }
--- IMS_GUI/SaleInstance.Designer.cs
+++ IMS_GUI/SaleInstance.Designer.cs
@@ -28,11 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCreateSale = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.cblAddon = new System.Windows.Forms.CheckedListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnCreateSale
+            // 
+            this.btnCreateSale.Location = new System.Drawing.Point(58, 456);
+            this.btnCreateSale.Name = "btnCreateSale";
+            this.btnCreateSale.Size = new System.Drawing.Size(151, 39);
+            this.btnCreateSale.TabIndex = 0;
+            this.btnCreateSale.Text = "Create Sale";
+            this.btnCreateSale.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(134, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(134, 250);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // cblAddon
+            // 
+            this.cblAddon.FormattingEnabled = true;
+            this.cblAddon.Location = new System.Drawing.Point(58, 143);
+            this.cblAddon.Name = "cblAddon";
+            this.cblAddon.Size = new System.Drawing.Size(245, 88);
+            this.cblAddon.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(58, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(245, 27);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(424, 163);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 97);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // SaleInstance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 507);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cblAddon);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCreateSale);
+            this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "SaleInstance";
             this.Text = "SaleInstance";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCreateSale;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckedListBox cblAddon;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListView listView1;
     }
 }
