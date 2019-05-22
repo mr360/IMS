f4bc5a36752acbd06c03b2f55a39d4d126a8d74b
mr360 <qd0097@gmail.com>
2019-05-22 14:58:34 +1000

    Created design for login form

--- IMS_GUI/Login.Designer.cs
+++ IMS_GUI/Login.Designer.cs
@@ -28,19 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblLoginHeading = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.Location = new System.Drawing.Point(59, 98);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(279, 19);
+            this.lblUsername.TabIndex = 0;
+            this.lblUsername.Text = "Please enter your IMS username";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(99, 189);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(189, 35);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(63, 130);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(275, 26);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // lblLoginHeading
+            // 
+            this.lblLoginHeading.AutoSize = true;
+            this.lblLoginHeading.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblLoginHeading.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoginHeading.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblLoginHeading.Location = new System.Drawing.Point(12, 23);
+            this.lblLoginHeading.Name = "lblLoginHeading";
+            this.lblLoginHeading.Size = new System.Drawing.Size(194, 28);
+            this.lblLoginHeading.TabIndex = 3;
+            this.lblLoginHeading.Text = "HTV IMS Portal";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(-7, -5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(430, 88);
+            this.panel1.TabIndex = 4;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(590, 298);
+            this.ClientSize = new System.Drawing.Size(405, 293);
+            this.Controls.Add(this.lblLoginHeading);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.panel1);
             this.Name = "Login";
             this.Text = "Login";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblLoginHeading;
+        private System.Windows.Forms.Panel panel1;
     }
 }
