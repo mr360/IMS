fbe94c9b8a464d2f2f0fc2688f60cf7a104ca4b5
mr360 <qd0097@gmail.com>
2019-05-25 07:19:54 +1000

    Added btnCreate code + name field

---
+++ IMS_GUI/CreateReportForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IMS;
+
+namespace IMS_GUI
+{
+    public partial class CreateReportForm : Form
+    {
+        public CreateReportForm()
+        {
+            InitializeComponent();
+            cbReportType.DataSource = Enum.GetValues(typeof(ReportType));
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            ReportInstanceForm.txtName = txtName.Text;
+            ReportInstanceForm.txtStartPeriod = txtPeriodStart.Text;
+            ReportInstanceForm.txtEndPeriod = txtPeriodEnd.Text;
+            ReportInstanceForm.txtReportType = cbReportType.SelectedText;
+            this.Close();
+        }
+
+        private void CreateReportForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
