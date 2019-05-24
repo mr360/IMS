0771d3139aaee8f6799d1f1396a3048dafc31261
mr360 <qd0097@gmail.com>
2019-05-25 07:17:43 +1000

    Retreieves invoices in db as either tax or sale.

--- IMS/Manager/InvoiceManager.cs
+++ IMS/Manager/InvoiceManager.cs
@@ -36,7 +36,26 @@ namespace IMS.Manager
         public override List<DbObject> RetrieveMany(string id)
         {
             // Tax / Sale
-            throw new NotImplementedException();
+            List<DbObject> lOutput = new List<DbObject>();
+            List<string> lIdList = _db.GetIDs;
+            foreach (string ids in lIdList)
+            {
+                Invoice.Invoice lInvoice = null;
+                switch (id.ToLower())
+                {
+                    case "tax":
+                        lInvoice = _db.Read(ids) as Tax;
+                        break;
+                    case "sale":
+                        lInvoice = _db.Read(ids) as Sale;
+                        break;
+                    default:
+                        throw new ArgumentException("Needs to be either tax or sale");
+                }
+
+                if (lInvoice != null) lOutput.Add(lInvoice);
+            }
+            return lOutput;
         }
     }
 }
