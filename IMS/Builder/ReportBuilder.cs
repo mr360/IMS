36227ce5d0d006194932ad01519799dd9cda5ffa
mr360 <qd0097@gmail.com>
2019-05-25 07:14:41 +1000

    Implemented ReportBuilder

--- IMS/Builder/ReportBuilder.cs
+++ IMS/Builder/ReportBuilder.cs
@@ -3,14 +3,120 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IMS.Manager;
+using IMS.Invoice;
+using IMS;
+
+namespace IMS
+{
+    public enum ReportType { Addon, TradeIn, Sale, None};
+}
 
 namespace IMS.Builder
 {
     public class ReportBuilder
     {
-        public  Object Prepare()
+        private string _name;
+        private DateTime _periodStart;
+        private DateTime _periodEnd;
+        private ReportType _reportType = ReportType.None;
+
+        private double _totalPrice;
+        private int _amountOfSale;
+
+        Report.Report _report;
+        Dictionary<string, IManager> _manager;
+
+        public ReportBuilder(Dictionary<string,IManager> manager)
         {
-            throw new NotImplementedException();
+            _manager = manager;
+        }
+
+        public void Period(string start, string end)
+        {
+            try
+            {
+                
+                _periodStart =  Convert.ToDateTime(start);
+                _periodEnd = Convert.ToDateTime(end);
+            }
+            catch
+            {
+                _periodStart.AddYears(1);
+                _periodEnd.AddYears(1);
+            }
+        }
+
+        public string Name
+        {
+            set
+            {
+                if (ValidateIMS.IsBad(value, @"^[a-zA-Z0-9]+$"))
+                {
+                    _name = null;
+                }
+                _name = value; 
+            }
+        }
+        public ReportType Type 
+        {
+            set
+            {
+                _reportType = value;
+            } 
+        }
+        public  string Prepare()
+        {
+            if (_reportType == ReportType.None || _periodStart.Year == 1 || _periodEnd.Year == 1 || String.IsNullOrEmpty(_name))
+            {
+                return "Fail.Need to enter correct values for report building.";
+            }
+
+            List<DbObject> lInvoiceList = _manager["Invoice"].RetrieveMany("tax");
+            _amountOfSale = lInvoiceList.Count();
+
+            foreach (Tax tInvoice in lInvoiceList)
+            {
+                if (!ValidDateRangeCheck(tInvoice, _periodStart, _periodEnd)) continue;
+                switch (_reportType)
+                {
+                    case ReportType.Addon:
+                        _totalPrice += tInvoice.AddonCost;
+                        break;
+                    case ReportType.Sale:
+                        _totalPrice += tInvoice.TotalCost;
+                        break;
+                    case ReportType.TradeIn:
+                        _totalPrice += tInvoice.TradeRebateCost;
+                        break;
+                }
+
+            }
+
+            _report = new Report.Report("UniqueIDReport", _name, _reportType, _periodStart, _periodEnd);
+            _report.TotalSalePrice = _totalPrice;
+            _report.AmountOfSale = _amountOfSale;
+            return "Success";
+           
+        }
+
+        private bool ValidDateRangeCheck(Invoice.Invoice t, DateTime start, DateTime end)
+        {
+            int lStart = t.Date.CompareTo(start);
+            int lEnd = t.Date.CompareTo(end);
+
+            // Less than zero ; earlier date
+            // Greater than zero; later date
+            if (lStart >= 0 && lEnd <= 0) return true;
+            return false;
+        }
+
+        public Report.Report Report
+        {
+            get
+            {
+                return _report;
+            }
         }
     }
 }
