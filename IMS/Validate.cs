a711634d7ccade711c1d88e190d7b6ad751b9217
mr360 <qd0097@gmail.com>
2019-05-26 08:06:53 +1000

    Moved validation out of the individual classes into its own

---
+++ IMS/Validate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IMS.User;
+using System.Text.RegularExpressions;
+
+namespace IMS
+{
+    public class ValidateIMS
+    {
+        public static bool IsBad(string t, string regex)
+        {
+            return !(Regex.IsMatch(t, regex));
+        }
+
+        public static bool IsValid(DbObject item)
+        {
+            if (item == null)
+            {
+                return false;
+            }
+
+            switch (item.GetType().Name)
+            {
+                case nameof(Vehicle):
+                    return IsVehicleValid(item as Vehicle);
+                case nameof(Addon):
+                    return IsAddonValid(item as Addon);
+                case nameof(Customer):
+                    return IsCustomerValid(item as Customer);
+                default: return false;
+            }
+        }
+
+        private static bool IsVehicleValid(Vehicle item)
+        {
+            if (ValidateIMS.IsBad(item.Id, @"^[a-zA-Z0-9]+$") || ValidateIMS.IsBad(item.Model, @"^[a-zA-Z0-9-]+$") || item.Price < 0.00)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsAddonValid(Addon item)
+        {
+            if (ValidateIMS.IsBad(item.Id, @"^[a-zA-Z0-9]+$") || ValidateIMS.IsBad(item.Name, @"^[a-zA-Z0-9-]+$") || item.Price < 0.00)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsCustomerValid(Customer item)
+        {
+            if (ValidateIMS.IsBad(item.Id, @"^[a-zA-Z0-9]+$") || ValidateIMS.IsBad(item.Name, @"^[a-zA-Z]+$") || ValidateIMS.IsBad(item.Address, @"^[a-zA-Z]+$"))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
+
