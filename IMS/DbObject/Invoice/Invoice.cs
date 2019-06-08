f49684381e801dc26769fae00c4d314b61bc3f66
mr360 <qd0097@gmail.com>
2019-06-09 00:41:14 +1000

    Restructured invoices. Invoice only contains header details. Main fuctions in Sales and Tax inheritits from Sales.

--- IMS/DbObject/Invoice/Invoice.cs
+++ IMS/DbObject/Invoice/Invoice.cs
@@ -14,13 +14,9 @@ namespace IMS.Invoice
 {
     public abstract class Invoice : DbObject
     {
-        protected DateTime _date;
-        protected Staff _salerep;
-        protected Customer _customer;
-        
-        protected Vehicle _buyVehicle;
-        protected Vehicle _tradeVehicle;
-        protected List<Addon> _addon = new List<Addon>();
+        private DateTime _date;
+        private Staff _salerep;
+
 
         public Invoice(string id, Staff saleRep) : base(id)
         {
@@ -32,7 +28,7 @@ namespace IMS.Invoice
         {
             get
             {
-               return _date;
+                return _date;
             }
         }
 
@@ -45,7 +41,7 @@ namespace IMS.Invoice
                        "SALE REP: " + SaleRep.Name + " " + SaleRep.Id + "\n" +
                        "COMPANY : HTV VEHICLE PTD\n" +
                        "COMPANY ADDRESS : 25 Malvin Street, VIC, 3000\n";
-                       
+
             }
         }
 
@@ -57,118 +53,6 @@ namespace IMS.Invoice
             }
         }
 
-        public Customer Customer
-        {
-            get
-            {
-                return _customer;
-            }
-        }
-
-        public bool Add(Addon a)
-        {
-            if (_buyVehicle == null || _addon.Contains(a)) return false;
-            _addon.Add(a);
-            return true;
-        }
-
-        public bool Add(VType type, Vehicle vehicle)
-        {
-            if ((type != VType.New && type != VType.TradeIn) || vehicle == null)
-            {
-                return false;
-            }
-
-            if (type == VType.New)
-            {
-                _buyVehicle = vehicle;
-            }
-            else
-            {
-                _tradeVehicle = vehicle;
-            }
-
-            return true;
-        }
-
-        public Vehicle TradeVehicle
-        {
-            get
-            {
-                return _tradeVehicle;
-            }
-        }
-
-        public Vehicle BuyVehicle
-        {
-            get
-            {
-                return _buyVehicle;
-            }
-        }
-
-        public List<Addon> Addon
-        {
-            get
-            {
-                return _addon;
-            }
-        }
-        public  double TotalCost
-        {
-            get
-            {
-                return VehicleCost + TradeRebateCost + AddonCost;
-            }
-        }
-        public double TradeRebateCost
-        {
-            get
-            {
-                if (_tradeVehicle == null)
-                {
-                    return 0.00;
-                }
-                return (-(_tradeVehicle.Price * 0.25));
-            }
-        }
-
-        public string ViewAllAddon
-        {
-            get
-            {
-                string s1 = "";
-                foreach (Addon a in _addon)
-                {
-                    s1 += a.View;
-                }
-
-                return s1;
-            }
-        }
-
-        public virtual double VehicleCost
-        {
-            get
-            {
-                return _buyVehicle.Price;
-            }
-        }
-
-
-        public virtual double AddonCost
-        {
-            get
-            {
-                double c = 0.0;
-                foreach (Addon a in _addon)
-                {
-                    c += a.Price;
-                }
-
-                return c;
-            }
-        }
-
+        public abstract double TotalCost {get;}
     }
 }
